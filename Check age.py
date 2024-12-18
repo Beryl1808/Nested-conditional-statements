2baa2e8fd64f5e808718eb2880b36912690fbab7
Beryl1808 <dkwafo17@gmail.com>
2024-12-18 18:37:12 +0000

    5cm

---
+++ Check age.py
@@ -0,0 +1,10 @@
+atten = int(input("enter the ages"))
+
+if age_number == "Y" :
+    print("You are allowed")
+else:
+    if atten >= 10 - 20:
+        print("Allowed")
+    
+if atten >= 1 - 9 :
+        print("Decline")
