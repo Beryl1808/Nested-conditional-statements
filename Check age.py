43472f7ac45ef27e0de476936aac738332482d5c
Beryl1808 <dkwafo17@gmail.com>
2024-12-18 20:04:06 +0000

    6cm

--- Check age.py
+++ Check age.py
@@ -1,9 +1,10 @@
+age_number=input("Are you 10 - 20 Y or N :")
 atten = int(input("enter the ages"))
 
 if age_number == "Y" :
     print("You are allowed")
 else:
-    if atten >= 10 - 20:
+    if atten <n= 10 - 20:
         print("Allowed")
     
 if atten >= 1 - 9 :
