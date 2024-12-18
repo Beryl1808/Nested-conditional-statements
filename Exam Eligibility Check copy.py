a5f42497222aa3dec2e38c3966734c584d62a415
Beryl1808 <dkwafo17@gmail.com>
2024-12-18 18:09:25 +0000

    cmm

---
+++ Exam Eligibility Check copy.py
@@ -0,0 +1,14 @@
+
+age_number=input("Are you 10 to 20years:")
+
+atten = int(input("enter the ages"))
+
+if age_number == "Y" :
+    print("You are allowed")
+else:
+    if atten >= 10 - 20:
+        print("Allowed")
+    
+if atten >= 1 - 9 :
+        print("Decline")
+         
